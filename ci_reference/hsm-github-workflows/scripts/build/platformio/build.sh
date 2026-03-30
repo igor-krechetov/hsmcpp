53cce8c1fc0c2b633942820f215f56276b01c305
igor-krechetov <58586005+igor-krechetov@users.noreply.github.com>
2026-03-30 23:26:14 +0100

    [ci] scripts test

--- ci_reference/hsm-github-workflows/scripts/build/platformio/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-build_dir="$1"
-build_type="$2"
-
-cmake --build "$build_dir" --config "$build_type" --target install
--- ci_reference/hsm-github-workflows/scripts/build/windows/build.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set BUILD_DIR=%1
-set BUILD_TYPE=%2
-
-cmake --build "%BUILD_DIR%" --parallel 2 --config %BUILD_TYPE%

b9e1674d105645c471f9433cdd6f18f28388033f
igor-krechetov <58586005+igor-krechetov@users.noreply.github.com>
2026-03-31 01:26:03 +0100

    [ci] fixes

--- scripts/test/ubuntu/test.sh
+++ scripts/test/ubuntu/test.sh
@@ -6,17 +6,21 @@ build_dir="$1"
 cd $build_dir
 
 echo "Run Tests (STD)"
+chmod +x ./tests/hsmUnitTestsSTD
 timeout 2m ./tests/hsmUnitTestsSTD > ./tests_result_std.log
 lcov -c -d . -o ./coverage_std.info
 
 echo "Run Tests (GLib)"
+chmod +x ./tests/hsmUnitTestsGLib
 timeout 2m ./tests/hsmUnitTestsGLib > ./tests_result_glib.log
 lcov -c -d . -o ./coverage_glib.info
 
 echo "Run Tests (GLibmm)"
+chmod +x ./tests/hsmUnitTestsGLibmm
 timeout 2m ./tests/hsmUnitTestsGLibmm > ./tests_result_glibmm.log
 lcov -c -d . -o ./coverage_glibmm.info
 
 echo "Run Tests (Qt)"
+chmod +x ./tests/hsmUnitTestsQt
 timeout 2m ./tests/hsmUnitTestsQt > ./tests_result_qt.log
 lcov -c -d . -o ./coverage_qt.info

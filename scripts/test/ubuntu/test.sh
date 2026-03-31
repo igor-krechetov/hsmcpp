f7d121f546412aabfc0e136876f16d347147527e
igor-krechetov <58586005+igor-krechetov@users.noreply.github.com>
2026-03-31 01:35:40 +0100

    [ci] fixes

--- scripts/test/ubuntu/test.sh
+++ scripts/test/ubuntu/test.sh
@@ -8,19 +8,19 @@ cd $build_dir
 echo "Run Tests (STD)"
 chmod +x ./tests/hsmUnitTestsSTD
 timeout 2m ./tests/hsmUnitTestsSTD > ./tests_result_std.log
-lcov -c -d . -o ./coverage_std.info
+lcov -c -d . -o ./coverage_std.info --ignore-errors mismatch
 
 echo "Run Tests (GLib)"
 chmod +x ./tests/hsmUnitTestsGLib
 timeout 2m ./tests/hsmUnitTestsGLib > ./tests_result_glib.log
-lcov -c -d . -o ./coverage_glib.info
+lcov -c -d . -o ./coverage_glib.info --ignore-errors mismatch
 
 echo "Run Tests (GLibmm)"
 chmod +x ./tests/hsmUnitTestsGLibmm
 timeout 2m ./tests/hsmUnitTestsGLibmm > ./tests_result_glibmm.log
-lcov -c -d . -o ./coverage_glibmm.info
+lcov -c -d . -o ./coverage_glibmm.info --ignore-errors mismatch
 
 echo "Run Tests (Qt)"
 chmod +x ./tests/hsmUnitTestsQt
 timeout 2m ./tests/hsmUnitTestsQt > ./tests_result_qt.log
-lcov -c -d . -o ./coverage_qt.info
+lcov -c -d . -o ./coverage_qt.info --ignore-errors mismatch

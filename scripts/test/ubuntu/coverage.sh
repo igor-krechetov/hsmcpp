35b306f5d3b51fb79e773b560bb844217371b697
igor-krechetov <58586005+igor-krechetov@users.noreply.github.com>
2026-03-31 23:34:56 +0100

    [ci] fixes

--- scripts/test/ubuntu/coverage.sh
+++ scripts/test/ubuntu/coverage.sh
@@ -11,8 +11,23 @@ lcov --add-tracefile ./coverage_std.info \
     -a ./coverage_qt.info \
     -a ./coverage_glibmm.info \
     -o ./coverage.info
-lcov -r ./coverage.info /usr/include/\* . -o ./coverage.info --ignore-errors empty --ignore-errors unused
-lcov -r ./coverage.info \*/build/\* . -o ./coverage.info --ignore-errors empty --ignore-errors unused
-lcov -r ./coverage.info \*/tests/\* . -o ./coverage.info --ignore-errors empty --ignore-errors unused
-lcov -r ./coverage.info \*/gcc_64/include/QtCore/\* . -o ./coverage.info --ignore-errors empty --ignore-errors unused
-lcov -r ./coverage.info \*/thirdparty/\* . -o ./coverage.info --ignore-errors empty --ignore-errors unused
+
+lcov -r ./coverage.info \
+        '/usr/include/*' \
+        '*/build/*' \
+        '*/tests/*' \
+        '*/gcc_64/include/QtCore/*' \
+        '*/thirdparty/*' \
+        -o ./coverage.info
+
+# lcov -r ./coverage.info '/usr/include/*' -o ./coverage.info
+# lcov -r ./coverage.info '*/build/*' -o ./coverage.info
+# lcov -r ./coverage.info '*/tests/*' -o ./coverage.info
+# lcov -r ./coverage.info '*/gcc_64/include/QtCore/*' -o ./coverage.info
+# lcov -r ./coverage.info '*/thirdparty/*' -o ./coverage.info
+
+# lcov -r ./coverage.info /usr/include/\* . -o ./coverage.info --ignore-errors empty --ignore-errors unused
+# lcov -r ./coverage.info \*/build/\* . -o ./coverage.info --ignore-errors empty --ignore-errors unused
+# lcov -r ./coverage.info \*/tests/\* . -o ./coverage.info --ignore-errors empty --ignore-errors unused
+# lcov -r ./coverage.info \*/gcc_64/include/QtCore/\* . -o ./coverage.info --ignore-errors empty --ignore-errors unused
+# lcov -r ./coverage.info \*/thirdparty/\* . -o ./coverage.info --ignore-errors empty --ignore-errors unused
